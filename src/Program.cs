63afcb3afed305d6a38b8b9c653831371d25f147
Jakub Fukala <xfukal01@stud.fit.vut.cz>
2024-04-20 16:45:25 +0200

    feat: Using UDP scanning in Main method

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,5 @@
 ﻿using ipk_l4_scan.Exeptions;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 
 namespace ipk_l4_scan
 {
@@ -59,64 +57,24 @@ namespace ipk_l4_scan
             {
                 Console.WriteLine($"{address} | {address.AddressFamily}");
             }
-            
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
             if (parser.InterfaceAddress == null)
             {
-                Environment.Exit(99);
+                Environment.Exit(1);
             }
             
-            // // This will establish full TCP connection with the target and send a message
-            // try
-            // {
-            //     // In this case, we are binding the socket to the interface address and port 0 (random port)
-            //     IPEndPoint endPoint = new IPEndPoint(parser.InterfaceAddress, 0);
-            //     socket.Bind(endPoint);
-            //     
-            //     Console.WriteLine($"Socket bound to {endPoint.Address}:{endPoint.Port}");
-            //     socket.Connect(parser.Target, 2000);
-            //     Console.WriteLine($"Connected to {parser.Target}");
-            //     
-            //     // Send a message to the target
-            //     string message = "Hello from scanner\n";
-            //     byte[] data = Encoding.ASCII.GetBytes(message);
-            //     int bytesSent = socket.Send(data);
-            //     
-            //     // Receive a response from the target
-            //     byte[] buffer = new byte[1024];
-            //     int bytesReceived = socket.Receive(buffer);
-            //     string response = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
-            //     Console.WriteLine($"Received: {response}");
-            //     
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine(e);
-            // }
-            // finally
-            // {
-            //     socket.Close();
-            // }
-            
-            
-            TcpSynScanner scanner = new TcpSynScanner("192.168.0.17", 33689, parser.Timeout);
-            PortScanResult result = scanner.Scan(parser.Target, 2000);
-            
-            switch (result)
+            UpdPortScanner scanner = new UpdPortScanner(parser.InterfaceAddress, parser.RetransmissionCount);
+            foreach (int port in parser.UdpPorts)
             {
-                case PortScanResult.Open:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Port {2000} is open");
-                    Console.ResetColor(); // Reset the color to default after printing the message
-                    break;
-                case PortScanResult.ClosedOrFiltered:
-                    Console.WriteLine($"Port {2000} is closed or filtered");
-                    break;
-                case PortScanResult.Error:
-                    Console.WriteLine("Error occurred during scanning");
-                    break;
+                UpdPortScanner.UdpPortScanResult result = scanner.Scan(addresses[0], port, parser.Timeout);
+                if (result == UpdPortScanner.UdpPortScanResult.Unreachable)
+                {
+                    Console.Error.WriteLine($"{parser.Target} is not reachable from {parser.InterfaceAddress}");
+                    Environment.Exit(5);
+                }
+                Console.WriteLine($"{port}/udp: {result}");
+                
             }
-            
         }
         
     }
